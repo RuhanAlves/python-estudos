89812522cb4aee2a09f91d74276ab143f51a21a4
Ruhan Alves <ruhanatcm@gmail.com>
2026-09-13 21:01:28 -0300

    Update ex073.py

--- exercicios/ex073.py
+++ exercicios/ex073.py
@@ -1,7 +1,6 @@
 time = ('Flamengo', 'Palmeiras', 'Atletica Parananese', 'Fluminense', 'Bahia', 'Cruzeiro', 'Coritiba', 'Atletico-MG', 'RB Bragantino', 'São Paulo', 'Vitoria', 'Corinthians', 'Santos', 'Botafogo', 'Gremio', 'Mirassol', 'Vasco Da Gama', 'Internacional', 'Remo', 'Chapecoense-sc')
 print(f'Cinco primeiros colocados do Brasileirão: \n{time[:5]}')
 print(f'Últimos quatro colocados: \n{time[-4:]}')
-#Falta a c, ordem alfabética
 print(f'Times em ordem alfabética: \n{sorted(time)}')
 for c in range (0, len(time)):
     if time[c] == 'Chapecoense-sc':
