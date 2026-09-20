540f67412136c5705c993800ae7e49c402c5fe9b
Ruhan Alves <ruhanatcm@gmail.com>
2026-09-20 18:58:26 -0300

    Update ex073.py

--- exercicios/ex073.py
+++ exercicios/ex073.py
@@ -1,7 +1,10 @@
-time = ('Flamengo', 'Palmeiras', 'Atletica Parananese', 'Fluminense', 'Bahia', 'Cruzeiro', 'Coritiba', 'Atletico-MG', 'RB Bragantino', 'São Paulo', 'Vitoria', 'Corinthians', 'Santos', 'Botafogo', 'Gremio', 'Mirassol', 'Vasco Da Gama', 'Internacional', 'Remo', 'Chapecoense-sc')
-print(f'Cinco primeiros colocados do Brasileirão: \n{time[:5]}')
-print(f'Últimos quatro colocados: \n{time[-4:]}')
-print(f'Times em ordem alfabética: \n{sorted(time)}')
-for c in range (0, len(time)):
-    if time[c] == 'Chapecoense-sc':
-        print(f'A Chapecoense se encontra na {c + 1}ª posição')
+time = ('Flamengo', 'Palmeiras', 'Atletica Parananese',
+        'Fluminense', 'Bahia', 'Cruzeiro', 'Coritiba',
+        'Atletico-MG', 'RB Bragantino', 'São Paulo',
+        'Vitoria', 'Corinthians', 'Santos', 'Botafogo',
+        'Gremio', 'Mirassol', 'Vasco Da Gama', 'Internacional',
+        'Remo', 'Chapecoense-sc')
+print(f'Cinco primeiros colocados do Brasileirão: {time[:5]}\n')
+print(f'Últimos quatro colocados: {time[-4:]}\n')
+print(f'Times em ordem alfabética: {sorted(time)}\n')
+print(f'A Chapecoense se encontra na {time.index("Chapecoense-sc") + 1}ª posição')
