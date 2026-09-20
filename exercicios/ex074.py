9fcddfc4be557672f294103e18860b39f986262e
Ruhan Alves <ruhanatcm@gmail.com>
2026-09-20 19:24:42 -0300

    Update ex074.py

--- exercicios/ex074.py
+++ exercicios/ex074.py
@@ -1,17 +1,6 @@
 from random import randint
-nums = ()
-maior = menor = 0
-for c in range (1, 6):
-    x = randint(1, 10)
-    if c == 1:
-        maior = x
-        menor = x
-    else:
-        if maior < x:
-            maior = x
-        if menor > x:
-            menor = x
-    nums = nums + (x,)
-print(f'Números gerados: {nums}')
-print(f'\nO maior número gerado foi \033[1;32m{maior}\033[m',end = '')
-print(f' e o menor foi \033[1;33m{menor}\033[m')
+numeros = (randint(1, 10), randint(1, 10), randint(1, 10),
+           randint(1, 10), randint(1, 10), )
+print(f'Valores sorteado: {numeros}')
+print(f'\nMaior valor sorteado: {max(numeros)}')
+print(f'\nMenor valor sorteado: {min(numeros)}')
